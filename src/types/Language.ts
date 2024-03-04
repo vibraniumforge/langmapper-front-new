471f3b05d3d14d0ed7ae1b72b0699f8793bee22b
Gregory Morgan <gregm9999@gmail.com>
2024-03-04 13:08:47 -0800

    fix typo

--- src/types/Language.ts
+++ src/types/Language.ts
@@ -1,15 +1,15 @@
-export interface Language{
-    id: number;
-    name: string;
-    abbreviation: string;
-    alphabet: string;
-    macrofamily: string;
-    family: string;
-    subfamily: string;
-    area1: string;
-    area2: string;
-    area3: string;
-    notel: string;
-    has_gender: boolean;
-    alive: boolean;
+export interface Language {
+  id: number;
+  name: string;
+  abbreviation: string;
+  alphabet: string;
+  macrofamily: string;
+  family: string;
+  subfamily: string;
+  area1: string;
+  area2: string;
+  area3: string;
+  notes: string;
+  has_gender: boolean;
+  alive: boolean;
 }
