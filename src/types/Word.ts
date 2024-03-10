b1cc519b524876f4184e28deb224cb4d8a1e2dbe
Gregory Morgan <gregm9999@gmail.com>
2024-03-10 07:43:14 -0700

    add category and emoji

--- src/types/Word.ts
+++ src/types/Word.ts
@@ -1,5 +1,7 @@
-export interface Word{
-    id: number;
-    word_name: string;
-    definition: string;
+export interface Word {
+  id: number;
+  word_name: string;
+  definition: string;
+  emoji?: string;
+  category: string;
 }
